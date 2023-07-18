dbb666206897a1188d48c66f84d5c52ca3481abc
Kosmo920 <kosmo9200@gmail.com>
2023-07-18 23:01:09 +0300

    Кнопка

---
+++ script.js
@@ -0,0 +1,8 @@
+function showTextField() {
+    var textField = document.createElement("input");
+    textField.setAttribute("type", "text");
+  
+    var parentElement = document.getElementById("myButton").parentNode;
+    parentElement.replaceChild(textField, document.getElementById("myButton"));
+  }
+  
