eb73f8ab351460ceea6db21313898d6dde034148
Kosmo920 <kosmo9200@gmail.com>
2023-07-19 00:01:43 +0300

    Кнопки 3.0

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function showTextField() {
+function CreateTextField() {
     var textField = document.createElement("input");
-    textField.setAttribute("type", "text");
-  
-    var parentElement = document.getElementById("myButton").parentNode;
-    parentElement.replaceChild(textField, document.getElementById("myButton"));
+    textField.setAttribute("type", "tel");
+    var div = document.getElementById("knop");
+    div.appendChild(textField);
+  }
 
+  function CreateTextField2() {
+    var textField2 = document.createElement("input");
+    textField2.setAttribute("type", "tel");
+    var div2 = document.getElementById("knop2");
+    div2.appendChild(textField2);
   }
   
-function showTextField2() {
-    var textField = document.createElement("input");
-    textField.setAttribute("type", "text");
-  
-    var parentElement = document.getElementById("myButton").parentNode;
-    parentElement.replaceChild(textField, document.getElementById("myButton"));
-
-  }
