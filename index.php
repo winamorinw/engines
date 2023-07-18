52371d1a965f900c61756f6aaebc6b121d0b20f9
Kosmo920 <kosmo9200@gmail.com>
2023-07-18 21:58:57 +0300

    123123

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php 
-phpinfo();
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+
+$conn = new mysqli($servername, $username, $password);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+
+$conn->close();
 ?>
