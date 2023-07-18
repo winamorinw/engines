5bb0296e60855f19f11fce561130c11a51d30e76
Kosmo920 <kosmo9200@gmail.com>
2023-07-18 23:16:30 +0300

    Кнопки 2.3

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ function showTextField() {
 
   }
   
+function showTextField2() {
+    var textField = document.createElement("input");
+    textField.setAttribute("type", "text");
+  
+    var parentElement = document.getElementById("myButton").parentNode;
+    parentElement.replaceChild(textField, document.getElementById("myButton"));
+
+  }
