5510e3027f8d3856b9bd7774f27aa69e03317845
Kosmo920 <kosmo9200@gmail.com>
2023-07-18 23:11:17 +0300

    Кнопки 2.2

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function showTextField() {
   
     var parentElement = document.getElementById("myButton").parentNode;
     parentElement.replaceChild(textField, document.getElementById("myButton"));
-//Кнопка 2
-    var parentElement = document.getElementById("myButton2").parentNode;
-    parentElement.replaceChild(textField, document.getElementById("myButton2"));
+
   }
   
