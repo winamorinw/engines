7b5986f342640d4c09affcf11a2de06a315ce30d
winamorinw <winamorinw@gmail.com>
2023-07-19 14:02:00 +0300

    автофокус при нажатии на +

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function CreateTextField() {
     var textField = document.createElement("input");
-    textField.setAttribute("type", "tel");
+    textField.setAttribute("type", "number");
+    textField.setAttribute("name", "wire");
     var div = document.getElementById("knop");
     div.appendChild(textField);
+    textField.focus();
   }
 
   function CreateTextField2() {
     var textField2 = document.createElement("input");
-    textField2.setAttribute("type", "tel");
+    textField2.setAttribute("type", "number");
+    textField2.setAttribute("name", "w");
     var div2 = document.getElementById("knop2");
     div2.appendChild(textField2);
+    textField2.focus();
   }
   
