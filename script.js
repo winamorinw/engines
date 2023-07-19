3ec463fecd669895ef93d16c92948634e7bea6f4
winamorinw <winamorinw@gmail.com>
2023-07-19 16:36:58 +0300

    добавление провода, фокус на инпут

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function CreateTextField() {
-    var textField = document.createElement("input");
-    textField.setAttribute("type", "number");
-    textField.setAttribute("name", "wire");
-    var div = document.getElementById("knop");
-    div.appendChild(textField);
-    textField.focus();
+    // let textField = document.createElement("input");
+    // textField.setAttribute("type", "number");
+    // let div = document.getElementById("knop");
+    // div.appendChild(textField);
+
+    let hiddeninput = document.querySelector('.hidden_input');
+    hiddeninput.setAttribute('class', '');
+    hiddeninput.focus();
   }
 
   function CreateTextField2() {
-    var textField2 = document.createElement("input");
-    textField2.setAttribute("type", "number");
-    textField2.setAttribute("name", "w");
-    var div2 = document.getElementById("knop2");
-    div2.appendChild(textField2);
-    textField2.focus();
+    // var textField2 = document.createElement("input");
+    // textField2.setAttribute("type", "number");
+    // var div2 = document.getElementById("knop2");
+    // div2.appendChild(textField2);
+
+    let hiddeninput = document.querySelector('.hidden_input');
+    hiddeninput.setAttribute('class', '');
+    hiddeninput.focus();
   }
   
